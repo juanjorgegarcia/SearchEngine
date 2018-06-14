d3bfef61a9182dd4350c3eba3f34899d3043c8c6
FabioAyresInsper <16002244+FabioAyresInsper@users.noreply.github.com>
2018-06-14 13:17:26 -0300

    Mudanças de legibilidade.

--- extractor.py
+++ extractor.py
@@ -5,22 +5,21 @@ class Extractor:
 
     def __init__(self,filepath):
         with open(filepath, "r") as f:
-            f = f.read()
+            self.string = f.read()
         # self.string = f.lower().replace(",.:/?!][","")
-        self.string = f
-
 
     def splitByParagraphs(self):
         paragraph = ""
         paragraphs = []
         self.paragraphs = {}
-        c=0
+        c = 0
         for i in range(len(self.string)):
-            paragraph+=self.string[i]
+            paragraph += self.string[i]
             if self.string[i-1] == "\n" and self.string[i] == "\n":
-                c+=1
+                c += 1
                 self.paragraphs[c] = paragraph
                 paragraph = ""
+
     def saveInJSON(self):
         with open('./data/raw/raw.json', 'w') as fp:
             json.dump(self.paragraphs, fp, sort_keys=True, indent=4)
@@ -30,6 +29,6 @@ class Extractor:
     #         pickle.dump(data, fp, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == "__main__":
-    oi = Extractor("./sherlock.txt")
-    oi.splitByParagraphs()
-    oi.saveInJSON()
+    extractor = Extractor("./sherlock.txt")
+    extractor.splitByParagraphs()
+    extractor.saveInJSON()
